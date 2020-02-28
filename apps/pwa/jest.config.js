e81f17452360cf56b39b11cc98c03827bab15941
miaborde <38043788+mIaborde@users.noreply.github.com>
2020-02-28 14:40:38 +0100

    🎉 Init: ng generate @nrwl/angular:application --name=pwa --style=scss --prefix=pwa

---
+++ apps/pwa/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'pwa',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/pwa',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
