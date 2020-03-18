4416b84386296930ba36f3f937dcc8c7861d00fd
miaborde <38043788+mIaborde@users.noreply.github.com>
2020-03-18 20:29:53 +0100

    📦 Add: ng add @angular/material

--- apps/pwa/src/app/app.module.ts
+++ apps/pwa/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export const routes: Routes = [];
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
